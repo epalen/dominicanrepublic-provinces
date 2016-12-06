19f38b9e7eb1794d98eccd436fdba1b3ec40cc36
Edward <edwardpalen@gmail.com>
2016-12-06 13:04:37 -0400

    Database Seeder
    Este archivo corre pobla los campos den las tablas de la BD, el comando para correr este proceso en Laravel es: php artisan db:seed.

---
+++ DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(ProvinceTableSeeder::class);
+        $this->call(MunicipalityTableSeeder::class);
+        $this->call(DistrictTableSeeder::class);
+    }
+}
